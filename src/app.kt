95f805627a4114a863c68aa536ce2651cea5ff48
Cironis Adam <adam.cironis@outlook.com>
2018-08-29 00:58:08 +0200

    Array sorting

--- src/app.kt
+++ src/app.kt
@@ -1,7 +1,18 @@
 fun main(args: Array<String>) {
 
     var maybeNumber : Int? = 15
-    maybeNumber = null
-    println(maybeNumber * 2)
+    maybeNumber?.let {
+        println(it)
+    }
+
+    var maybeText : String? = "Hello world."
+    println(maybeText?.length ?: 0)
+
+    val simpsons : Array<String> = arrayOf("Homer", "Marge", "Bart", "Lisa", "Maggie")
+    simpsons.sort()
+    simpsons.reverse()
+    for (simpson in simpsons) {
+        println(simpson)
+    }
 
 }
