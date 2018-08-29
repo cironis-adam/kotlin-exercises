b38af3764ee28cb77bb7972372f86082014473fd
Cironis Adam <adam.cironis@outlook.com>
2018-08-29 13:06:49 +0200

    Morse code decoder

---
+++ src/morse_code.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+
+    // initializing variables
+    val alphabet : String = "abcdefghijklmnopqrstuvwxyz"
+    val morseSymbols : Array<String> = arrayOf(".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..")
+
+    var message : String = " ..   ..-.  ---  .-.  --.  ---  -   -  ---   -...  ..-  -.--   .-   -.-.  ---  ..-.  ..-.  .  . "
+    var decodedMessage : String = ""
+
+    // break message into morse code symbols
+    val symbols = message.split(" ")
+
+    // loop through the message
+    for (symbol in symbols) {
+        val index = morseSymbols.indexOf(symbol)
+
+        // check if symbol found
+        if (index != -1) {
+            decodedMessage += alphabet[index]
+        }
+    }
+
+    println("Message: $message")
+    println("Coded message: $decodedMessage")
+    
+}
